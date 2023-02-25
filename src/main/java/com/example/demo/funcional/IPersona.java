0ca933c3365c1930fcfb411c6da1ffc93a5a71d3
Henry17C <hicoyago@uce.edu.ec>
2023-02-24 19:47:31 -0500

    unidad 4

---
+++ src/main/java/com/example/demo/funcional/IPersona.java
@@ -0,0 +1,12 @@
+package com.example.demo.funcional;
+
+
+	@FunctionalInterface
+public interface IPersona {
+
+	public String comer();
+
+	//solo puedo colocar un solo metodo ya que es funcional
+	
+
+}
